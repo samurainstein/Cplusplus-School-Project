090e5bb11748beca889dc81871271e1092c28f3e
Eric Matelyan <eric.matelyan@gmail.com>
2021-03-27 12:06:32 -0400

    Initial roster.h setup

--- roster.h
+++ roster.h
@@ -1,1 +1,32 @@
 #pragma once
+#include "degree.h"
+#include "student.h"
+#include <string>
+using namespace std;
+
+class Roster {
+	public:
+		Roster();
+		~Roster();
+
+		void add(
+			string studentID,
+			string firstName,
+			string lastName,
+			string emailAddress,
+			int age,
+			int daysInCourse1,
+			int daysInCourse2,
+			int daysInCourse3,
+			DegreeProgram degreeProgram
+		);
+
+		void remove(string studentID);
+		void printAll();
+		void printAverageDaysInCourse(string studentID);
+		void printInvalidEmails();
+		void printByDegreeProgram();
+
+	private:
+		Student* classRosterArray[5];
+};
